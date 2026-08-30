2131783e6178f12c20727ad36f8886327dcfe14c
Prashant-so <140810707+Prashant-so@users.noreply.github.com>
2026-08-30 11:33:23 +0530

    Create ConfigManager.java

---
+++ src/main/java/dev/momentcraft/config/ConfigManager.java
@@ -0,0 +1,42 @@
+package dev.momentcraft.config;
+
+import dev.momentcraft.plugin.MomentCraftPlugin;
+import org.bukkit.configuration.file.FileConfiguration;
+
+public final class ConfigManager {
+
+    private final MomentCraftPlugin plugin;
+    private FileConfiguration config;
+
+    public ConfigManager(MomentCraftPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public void load() {
+        plugin.saveDefaultConfig();
+        plugin.reloadConfig();
+        config = plugin.getConfig();
+
+        int version = config.getInt("config-version", 0);
+        if (version != 1) {
+            plugin.getLogger().warning(
+                "config.yml has config-version " + version + ", expected 1. " +
+                "Some settings may not apply correctly until the file is updated."
+            );
+        }
+    }
+
+    public void reload() {
+        plugin.reloadConfig();
+        config = plugin.getConfig();
+        plugin.getLogger().info("Configuration reloaded.");
+    }
+
+    public boolean isDebug() {
+        return config.getBoolean("general.debug", false);
+    }
+
+    public FileConfiguration raw() {
+        return config;
+    }
+}
