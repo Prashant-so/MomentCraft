07252eac02cbee71d22e4716b743d428e4299757
Prashant-so <140810707+Prashant-so@users.noreply.github.com>
2026-08-30 11:01:30 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,6 @@
-plugins {
-    id("java")
-    id("com.gradleup.shadow") version "8.3.5"
-}
-
-group = "dev.momentcraft"
-version = "0.1.0-SNAPSHOT"
-description = "Automated cinematic moment detection and rendering for Paper servers."
-
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+        languageVersion.set(JavaLanguageVersion.of(25))
     }
 }
 
@@ -19,27 +10,5 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
-}
-
-tasks {
-    compileJava {
-        options.encoding = "UTF-8"
-        options.release.set(21)
-    }
-
-    processResources {
-        filesMatching("plugin.yml") {
-            expand("version" to project.version)
-        }
-    }
-
-    shadowJar {
-        archiveClassifier.set("")
-        archiveBaseName.set("MomentCraft")
-    }
-
-    build {
-        dependsOn(shadowJar)
-    }
+    compileOnly("io.papermc.paper:paper-api:26.2.build.+")
 }
